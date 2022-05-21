3cfafa83796e4407813d939d777c947e16a357a0
Tai <4382091+taisukemino@users.noreply.github.com>
2022-05-21 20:34:40 +0900

    buyCoverge method

--- contracts/core/tranche/Tranche.sol
+++ contracts/core/tranche/Tranche.sol
@@ -2,12 +2,13 @@
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "./LPToken.sol";
 import "../../interfaces/IReferenceLendingPools.sol";
 import "../../interfaces/IPremiumPricing.sol";
 
 /// @notice Tranche coordinates a swap market in between a buyer and a seller. It stores premium from a swap buyer and coverage capital from a swap seller.
-contract Tranche is LPToken {
+contract Tranche is LPToken, ReentrancyGuard {
   /*** libraries ***/
   /// @notice OpenZeppelin library for managing counters.
   using Counters for Counters.Counter;
@@ -21,6 +22,11 @@ contract Tranche is LPToken {
     IReferenceLendingPools referenceLendingPools
   );
 
+  /// @notice Emitted when a new buyer account is created.
+  event BuyerAccountCreated(address owner, uint256 accountId);
+
+  /// @notice Emitted when a new coverage is bought.
+  event CoverageBought(address buyer, uint256 lendingPoolId, uint256 premium);
   /*** struct definition ***/
 
   /*** variables ***/
@@ -33,8 +39,21 @@ contract Tranche is LPToken {
   /// @notice ReferenceLendingPools contract address
   IReferenceLendingPools public referenceLendingPools;
 
+  /// @notice The total amount of premium accumulated in the tranche
+  uint256 private _premiumTotal;
+
   /// @notice Buyer account id counter
   Counters.Counter public buyerAccountIdCounter;
+
+  /// @notice The buyer account id for each address
+  mapping(address => uint256) public ownerAddressToBuyerAccountId;
+
+  /// @notice The premium amount for each lending pool for each account id
+  /// @dev a buyer account id to a lending pool id to the premium amount
+  mapping(uint256 => mapping(uint256 => uint256)) public buyerAccounts;
+
+  /// @notice The total amount of premium for each lending pool
+  mapping(uint256 => uint256) public lendingPoolIdToPremiumTotal;
 
   /*** constructor ***/
   /**
@@ -64,6 +83,41 @@ contract Tranche is LPToken {
       _referenceLendingPools
     );
   }
+  /**
+   * @param _lendingPoolId The id of the lending pool.
+   */
+  modifier whenNotExpired(uint256 _lendingPoolId) {
+    // require(referenceLendingPools.checkIsExpired(uint256 _lendingPoolId) == false, "Lending pool has expired");
+    _;
+  }
+
+  /**
+   * @param _lendingPoolId The id of the lending pool.
+   */
+  modifier whenNotDefault(uint256 _lendingPoolId) {
+    // require(referenceLendingPools.checkIsDefaulted(_lendingPoolId) == false, "defaulted");
+    _;
+  }
+
+  modifier noBuyerAccountExist() {
+    require(
+      ownerAddressToBuyerAccountId[msg.sender] == 0,
+      "Tranche::noBuyerAccountExist: the buyer account already exists!"
+    );
+    _;
+  }
+
+  /**
+   * @notice The total amount of premium in the tranche.
+   */
+  function premiumTotal() public view returns (uint256) {
+    return _premiumTotal;
+  }
+
+  function _noBuyerAccountExist() private view returns (bool) {
+    return ownerAddressToBuyerAccountId[msg.sender] == 0;
+  }
+
   /*** state-changing functions ***/
   /// @notice allows the owner to pause the contract
   function pauseTranche() external onlyOwner {
@@ -73,5 +127,47 @@ contract Tranche is LPToken {
   /// @notice allows the owner to unpause the contract
   function unpauseTranche() external onlyOwner {
     _unpause();
+  }
+  /**
+   * @notice Create a unique account of a coverage buyer for an EOA
+   * @dev Only one account can be created per EOA
+   */
+  function _createBuyerAccount() private noBuyerAccountExist whenNotPaused {
+    uint256 _accountId = buyerAccountIdCounter.current();
+    ownerAddressToBuyerAccountId[msg.sender] = _accountId;
+    buyerAccountIdCounter.increment();
+    emit BuyerAccountCreated(msg.sender, _accountId);
+  }
+
+  /**
+   * @dev The paymentToken must be approved first.
+   * @param _lendingPoolId The id of the lending pool to be covered.
+   * @param _expirationTime For how long you want to cover.
+   * @param _coverageAmount How much you want to cover.
+   */
+  function buyCoverage(
+    uint256 _lendingPoolId,
+    uint256 _expirationTime,
+    uint256 _coverageAmount
+  )
+    external
+    whenNotExpired(_lendingPoolId)
+    whenNotDefault(_lendingPoolId)
+    whenNotPaused
+    nonReentrant
+  {
+    if (_noBuyerAccountExist() == true) {
+      _createBuyerAccount();
+    }
+    uint256 _premiumAmount = premiumPricing.calculatePremium(
+      _expirationTime,
+      _coverageAmount
+    );
+    uint256 _accountId = ownerAddressToBuyerAccountId[msg.sender];
+    buyerAccounts[_accountId][_lendingPoolId] += _premiumAmount;
+    paymentToken.transferFrom(msg.sender, address(this), _premiumAmount);
+    lendingPoolIdToPremiumTotal[_lendingPoolId] += _premiumAmount;
+    _premiumTotal += _premiumAmount;
+    emit CoverageBought(msg.sender, _lendingPoolId, _premiumAmount);
   }
 }
--- test/contracts/Tranche.test.ts
+++ test/contracts/Tranche.test.ts
@@ -1,3 +1,4 @@
+import { BigNumber } from "@ethersproject/bignumber";
 import { expect } from "chai";
 import { Contract, Signer } from "ethers";
 import { USDC_ADDRESS, USDC_DECIMALS, USDC_ABI } from "../utils/constants";
@@ -52,6 +53,145 @@ const tranche: Function = (
         expect(_premiumPricingAddress).to.eq(USDC_ADDRESS);
       });
     });
+
+    describe("buyCoverage", () => {
+      it("...should expire the referenceLoans", async () => {
+        // write this test once you finish writing the ReferenceLendingPools contract
+        //   await referenceLoans.setIsExpired(true);
+        //   const _checkIsExpired: boolean =
+        //     await referenceLoans.checkIsExpired();
+        //   expect(_checkIsExpired.toString()).to.eq("true");
+      });
+
+      it("...fails if the lending pool has expired already", async () => {
+        // write this test once you finish writing the ReferenceLendingPools contract
+        // expect(await tranche.buyCoverage(0,0,0)).to.be.revertedWith("Lending pool has expired");
+      });
+
+      it("...should roll back the expiration the referenceLoans for testing", async () => {
+        // write this test once you finish writing the ReferenceLendingPools contract
+        //   await referenceLoans.setIsExpired(true);
+        //   const _checkIsExpired: boolean =
+        //     await referenceLoans.checkIsExpired();
+        //   expect(_checkIsExpired.toString()).to.eq("true");
+      });
+
+      it("...fails if the lending pool has defaulted", async () => {
+        // write this test once you finish writing the ReferenceLendingPools contract
+        // even if a pool defaults, there may be more default from other loans in the pool. whenNotDefault should be valid only when all the loans in the lending pool default?
+      });
+
+      it("...fails if the Tranche contract is paused", async () => {
+        await tranche.pauseTranche();
+        await expect(tranche.buyCoverage(0, 0, 0))
+          .to.emit(tranche, "Paused")
+          .withArgs(deployerAddress)
+          .to.be.revertedWith("Pausable: paused");
+        expect(await tranche.paused()).to.be.true;
+      });
+
+      it("...unpause the Tranche contract", async () => {
+        await tranche.unpauseTranche();
+        expect(await tranche.paused()).to.be.false;
+      });
+
+      it("...reentrancy should fail", async () => {
+        // write this test later
+      });
+
+      it("...the buyer account doesn't exist for the msg.sender", async () => {
+        const _noBuyerAccountExist: boolean =
+          (await tranche
+            .ownerAddressToBuyerAccountId(deployerAddress)
+            .toString()) === "0";
+        expect(_noBuyerAccountExist).to.eq(true);
+      });
+
+      it("...fail if USDC is not approved", async () => {
+        expect(await tranche.buyCoverage(0, 0, 0)).to.be.revertedWith(
+          "ERC20: insufficient allowance"
+        );
+      });
+
+      it("...approve 100 USDC to be transferred by the Tranche contract", async () => {
+        expect(
+          await USDC.approve(
+            tranche.address,
+            BigNumber.from(100).mul(USDC_DECIMALS)
+          )
+        )
+          .to.emit(USDC, "Approval")
+          .withArgs(
+            deployerAddress,
+            tranche.address,
+            BigNumber.from(100).mul(USDC_DECIMALS)
+          );
+
+        const _allowanceAmount: number = await USDC.allowance(
+          deployerAddress,
+          tranche.address
+        );
+        await expect(_allowanceAmount.toString()).to.eq(
+          BigNumber.from(100).mul(USDC_DECIMALS).toString()
+        );
+      });
+
+      it("...create a new buyer account and buy coverage", async () => {
+        const _buyerAccountId: BigNumber =
+          await tranche.ownerAddressToBuyerAccountId(deployerAddress);
+        const _lendingPoolId: string = "0";
+        let _expirationTime: BigNumber = getUnixTimestampOfSomeMonthAhead(3);
+        let _coverageAmount: BigNumber = BigNumber.from(10).mul(
+          BigNumber.from(10).pow(USDC_DECIMALS)
+        );
+
+        const _premiumAmount: BigNumber = await premiumPricing.calculatePremium(
+          _expirationTime,
+          _coverageAmount
+        );
+
+        const _accountId: BigNumber =
+          await tranche.ownerAddressToBuyerAccountId(deployerAddress);
+        const _premiumAmountOfAccountBefore: BigNumber =
+          await tranche.buyerAccounts(_accountId, _lendingPoolId);
+        const _premiumTotalOfLendingPoolIdBefore: BigNumber =
+          await tranche.lendingPoolIdToPremiumTotal(_lendingPoolId);
+        const _premiumTotalBefore: BigNumber = await tranche.premiumTotal();
+
+        await expect(
+          tranche.buyCoverage(_lendingPoolId, _expirationTime, _coverageAmount)
+        )
+          .to.emit(tranche, "BuyerAccountCreated")
+          .withArgs(deployerAddress, _buyerAccountId)
+          .to.emit(tranche, "CoverageBought")
+          .withArgs(deployerAddress, _lendingPoolId, _premiumAmount);
+
+        const _premiumAmountOfAccountAfter: BigNumber =
+          await tranche.buyerAccounts(_accountId, _lendingPoolId);
+        const _premiumTotalOfLendingPoolIdAfter: BigNumber =
+          await tranche.lendingPoolIdToPremiumTotal(_lendingPoolId);
+        const _premiumTotalAfter: BigNumber = await tranche.premiumTotal();
+
+        expect(_premiumAmountOfAccountBefore.add(_premiumAmount)).to.eq(
+          _premiumAmountOfAccountAfter
+        );
+        expect(_premiumTotalBefore.add(_premiumAmount)).to.eq(
+          _premiumTotalAfter
+        );
+        expect(_premiumTotalOfLendingPoolIdBefore.add(_premiumAmount)).to.eq(
+          _premiumTotalOfLendingPoolIdAfter
+        );
+      });
+
+      it("...the buyer account for the msg.sender exists already", async () => {
+        const _noBuyerAccountExist: boolean =
+          (await tranche
+            .ownerAddressToBuyerAccountId(deployerAddress)
+            .toString()) === "0";
+        expect(_noBuyerAccountExist).to.eq(false);
+      });
+    });
+
     describe("pauseTranche", () => {
       it("...should allow owner to pause contract", async () => {
         await expect(
