cd9cbd91d9145390e46b60a993a487bf401c109f
Tai <4382091+taisukemino@users.noreply.github.com>
2022-06-22 11:58:28 +0900

    premium pricing contract test file

---
+++ test/contracts/PremiumPricing.test.ts
@@ -0,0 +1,9 @@
+import { Signer } from "ethers";
+import { PremiumPricing } from "../../typechain-types/contracts/core/PremiumPricing";
+
+const premiumPricing: Function = (
+  deployer: Signer,
+  premiumPricing: PremiumPricing
+) => {};
+
+export { premiumPricing };
