374e4bc09840f8cadf5b6480ce59e6011ea5d464
Tai <4382091+taisukemino@users.noreply.github.com>
2022-08-17 12:57:56 +0900

    renaming

--- test/utils/usdc.ts
+++ test/utils/usdc.ts
@@ -1,13 +1,13 @@
 import { BigNumber } from "@ethersproject/bignumber";
 import { formatUnits, parseUnits } from "ethers/lib/utils";
-import { USDC_NO_OF_DECIMALS } from "./constants";
+import { USDC_NUM_OF_DECIMALS } from "./constants";
 
 const formatUSDC: Function = (usdcAmt: BigNumber): string => {
-  return formatUnits(usdcAmt, USDC_NO_OF_DECIMALS);
+  return formatUnits(usdcAmt, USDC_NUM_OF_DECIMALS);
 };
 
 const parseUSDC: Function = (usdcAmtText: string): BigNumber => {
-  return parseUnits(usdcAmtText, USDC_NO_OF_DECIMALS);
+  return parseUnits(usdcAmtText, USDC_NUM_OF_DECIMALS);
 };
 
 export { formatUSDC, parseUSDC };
